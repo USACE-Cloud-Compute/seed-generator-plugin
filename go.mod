5a943d21d41e3b7c79d0f50ec81cb34ea09c7613
Will Lehman <william.p.lehman@usace.army.mil>
2022-07-16 23:26:52 +0000

    testing the basic functionality of the new wat-go-sdk plugin package, found issue with the json configurations

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/henrygeorgist/eventgenerator
 
 go 1.15
+
+require github.com/usace/wat-go-sdk v0.0.0-20220716113352-806122af8731
--- main.go
+++ main.go
@@ -3,42 +3,56 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
+
+	"github.com/usace/wat-go-sdk/plugin"
 )
 
 func main() {
 	fmt.Println("event generator!")
-	var payload string
-	flag.StringVar(&payload, "payload", "", "please specify an input file using `-payload=pathtopayload.yml`")
+	var payloadPath string
+	flag.StringVar(&payloadPath, "payload", "", "please specify an input file using `-payload=pathtopayload.yml`")
 	flag.Parse()
-
-	if payload == "" {
-		fmt.Println("given a blank path...")
-		fmt.Println("please specify an input file using `-payload=pathtopayload.yml`")
+	if payloadPath == "" {
+		plugin.Log(plugin.Message{
+			Status:    plugin.FAILED,
+			Progress:  0,
+			Level:     plugin.ERROR,
+			Message:   "given a blank path...\n\tplease specify an input file using `-payload=pathtopayload.yml`",
+			Sender:    "eventgenerator",
+			PayloadId: "unknown payloadid because the plugin package could not be properly initalized",
+		})
 		return
 	}
-	payloadInstructions, err := utils.LoadModelPayloadFromS3(payload, fs)
+	err := plugin.InitConfigFromPath("howDoIGetThePluginConfigPassedIn?")
 	if err != nil {
-		fmt.Println("not successful", err)
+		plugin.Log(plugin.Message{
+			Status:    plugin.FAILED,
+			Progress:  0,
+			Level:     plugin.ERROR,
+			Message:   err.Error(),
+			Sender:    "eventgenerator",
+			PayloadId: "unknown payloadid because the plugin package could not be properly initalized",
+		})
 		return
 	}
-
-	message := fmt.Sprintf("Fragility Curve Complete %v", val)
-	fmt.Println("sending message: " + message)
-	queueURL := fmt.Sprintf("%v/queue/events", queue.Endpoint)
-	fmt.Println("sending message to:", queueURL)
-	_, err = queue.SendMessage(&sqs.SendMessageInput{
-		DelaySeconds: aws.Int64(1),
-		MessageBody:  aws.String(message),
-		QueueUrl:     &queueURL,
+	payload, err := plugin.LoadPayload(payloadPath)
+	if err != nil {
+		plugin.Log(plugin.Message{
+			Status:    plugin.FAILED,
+			Progress:  0,
+			Level:     plugin.ERROR,
+			Message:   err.Error(),
+			Sender:    "eventgenerator",
+			PayloadId: "unknown payloadid because the payload did not load",
+		})
+		return
+	}
+	plugin.Log(plugin.Message{
+		Status:    plugin.SUCCEEDED,
+		Progress:  100,
+		Level:     plugin.INFO,
+		Message:   "awwww yeah",
+		Sender:    "eventgenerator",
+		PayloadId: payload.Id,
 	})
-	key := payloadInstructions.PluginImageAndTag + "_" + payloadInstructions.Name + "_R" + fmt.Sprint(payloadInstructions.Realization.Index) + "_E" + fmt.Sprint(payloadInstructions.Event.Index)
-	out := red.Set(key, "complete", 0)
-	fmt.Println(out)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	fmt.Println(message)
-	return
 }
