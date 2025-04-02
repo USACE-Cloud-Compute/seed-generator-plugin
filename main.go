afa8ad5a33731899507f8fc30554ed2b2a113e28
Will Lehman <william.p.lehman@usace.army.mil>
2025-04-02 20:50:40 +0000

    testing out tiledb

--- main.go
+++ main.go
@@ -169,6 +169,8 @@ func generateAllSeedsFromBlocks(action cc.Action, pm *cc.PluginManager) error {
 	var blocks []blockgeneratormodel.Block
 	block_name := action.Attributes.GetStringOrFail("block_dataset_name")
 	blockStoreType := action.Attributes.GetStringOrFail("block_store_type")
+	seed_name := action.Attributes.GetStringOrFail("seed_dataset_name")
+	seedStoreType := action.Attributes.GetStringOrFail("seed_store_type")
 	if blockStoreType == "eventstore" {
 		recordset, err := cc.NewEventStoreRecordset(pm, &blocks, "eventstore", block_name)
 		if err != nil {
@@ -191,7 +193,7 @@ func generateAllSeedsFromBlocks(action cc.Action, pm *cc.PluginManager) error {
 	} else {
 
 		blockreader, err := action.IOManager.GetReader(cc.DataSourceOpInput{
-			DataSourceName: "blockfile",
+			DataSourceName: block_name,
 			PathKey:        "default",
 		})
 		if err != nil {
@@ -210,20 +212,96 @@ func generateAllSeedsFromBlocks(action cc.Action, pm *cc.PluginManager) error {
 		return err
 	}
 
-	bytedata, err := json.Marshal(modelResult)
-	if err != nil {
-		return err
-	}
-	breader := bytes.NewReader(bytedata)
-	_, err = action.Put(cc.PutOpInput{
-		SrcReader: breader,
-		DataSourceOpInput: cc.DataSourceOpInput{
-			DataSourceName: "seeds",
-			PathKey:        "default",
-		},
-	})
-	if err != nil {
-		return err
+	if seedStoreType == "eventstore" {
+		//get the store
+		ds, err := pm.GetStore(seedStoreType)
+		//check if it is the right store type
+		tdbds, ok := ds.Session.(cc.MultiDimensionalArrayStore)
+		if ok {
+			arrayInput := cc.CreateArrayInput{
+				ArrayPath: seed_name,
+				Dimensions: []cc.ArrayDimension{
+					{
+						Name:          "Plugins",
+						DimensionType: cc.DIMENSION_STRING,
+						Domain:        []int64{1, int64(len(plugins))},
+						TileExtent:    int64(len(plugins)),
+					}, {
+						Name:          "Events",
+						DimensionType: cc.DIMENSION_INT,
+						Domain:        []int64{1, int64(len(modelResult))},
+						TileExtent:    1,
+					}, /*{
+						Name: "Seeds",
+						DimensionType: cc.DIMENSION_INT,
+						Domain: []int64{1,2},
+						TileExtent: 1,//what does this mean really.
+					}*/
+				},
+				Attributes: []cc.ArrayAttribute{
+					{Name: "realization_seed", DataType: cc.ATTR_INT64},
+					{Name: "event_seed", DataType: cc.ATTR_INT64},
+				},
+				ArrayType:  cc.ARRAY_DENSE,
+				CellLayout: cc.ROWMAJOR,
+				TileLayout: cc.COLMAJOR,
+			}
+			err = tdbds.CreateArray(arrayInput)
+			if err != nil {
+				return err
+			}
+
+			//now make a put array input and put the data properly arranged.
+			eventseeddata := make([]int64, len(plugins)*len(modelResult))
+			realizationseeddata := make([]int64, len(plugins)*len(modelResult))
+			for i, ec := range modelResult {
+				for j, plugin := range plugins {
+					ss := ec.Seeds[plugin]
+					eventseeddata[i*len(plugin)+j] = ss.EventSeed
+					realizationseeddata[i*len(plugin)+j] = ss.RealizationSeed
+				}
+			}
+			//create a buffer
+			buffer := []cc.PutArrayBuffer{
+				{
+					AttrName: "event_seed",
+					Buffer:   eventseeddata,
+				},
+				{
+					AttrName: "realization_seed",
+					Buffer:   realizationseeddata,
+				},
+			}
+			//create an input
+			input := cc.PutArrayInput{
+				Buffers:   buffer,
+				DataPath:  seed_name,
+				ArrayType: cc.ARRAY_DENSE,
+			}
+			err = tdbds.PutArray(input)
+			if err != nil {
+				return err
+			}
+		} else {
+			//store does not support this data.
+			return errors.New("store session is not a multidimensional array store.")
+		}
+	} else {
+		bytedata, err := json.Marshal(modelResult)
+		if err != nil {
+			return err
+		}
+		breader := bytes.NewReader(bytedata)
+		_, err = action.Put(cc.PutOpInput{
+			SrcReader: breader,
+			DataSourceOpInput: cc.DataSourceOpInput{
+				DataSourceName: seed_name,
+				PathKey:        "default",
+			},
+		})
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
